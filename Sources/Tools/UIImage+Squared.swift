090bdde9f11af628c3307c1a8d162801df2e2e66
nidegen <ndegen@me.com>
2021-04-01 15:14:42 +0200

    fix squared image

--- Sources/Tools/UIImage+Squared.swift
+++ Sources/Tools/UIImage+Squared.swift
@@ -10,20 +10,20 @@ extension UIImage {
   var newY: CGFloat { isPortrait  ? ((size.height - size.width)/2).rounded(.down) : 0 }
   
   public func squared(size: CGFloat? = nil, isOpaque: Bool = false) -> UIImage? {
-    var image: UIImage
-    if let size = size, let resized = self.resize(clampingMin: size) {
-      image = resized
-    } else {
-      image = self
-    }
-    guard let cgImage = image.cgImage?
+    guard let cgImage = cgImage?
             .cropping(to: .init(origin: .init(x: newX, y: newY),
                                 size: breadthSize)) else { return nil }
     let format = imageRendererFormat
     format.opaque = isOpaque
-    return UIGraphicsImageRenderer(size: breadthSize, format: format).image { _ in
+    let squared = UIGraphicsImageRenderer(size: breadthSize, format: format).image { _ in
       UIImage(cgImage: cgImage, scale: 1, orientation: imageOrientation)
         .draw(in: .init(origin: .zero, size: breadthSize))
+    }
+    
+    if let size = size, let resized = squared.resize(clampingMin: size) {
+      return resized
+    } else {
+      return squared
     }
   }
 }
