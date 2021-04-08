46c28cf6e4d2580ad0cc7c97812b5594e8d1d791
nidegen <ndegen@me.com>
2021-04-08 07:36:27 +0200

    wip

--- Sources/Show/Uploader/Uploader.swift
+++ Sources/Show/Uploader/Uploader.swift
@@ -17,21 +17,21 @@ class Uploader: ObservableObject {
   
   @discardableResult
   public func uploadNewImage(_ photo: UIImage, id: Id = UUID().uuidString,
-                             maxResolution: CGFloat? = nil, compression: CGFloat = 0.5, completion: Completion? = nil) -> Id {
+                             maxResolution: CGFloat? = nil, compression: CGFloat = 0.5, completion: Completion? = nil) -> UploadTask {
     let task = server.uploadNewImage(photo, id: id, maxResolution: maxResolution, compression: compression, completion: completion)
     uploads.append(task)
     self.cache.setImage(photo, forId: id)
-    return id
+    return task
   }
   
   
   @discardableResult
-  public func uploadNewImage(fromURL photoURL: URL, id: Id = UUID().uuidString, completion: Completion? = nil) -> Id {
+  public func uploadNewImage(fromURL photoURL: URL, id: Id = UUID().uuidString, completion: Completion? = nil) -> UploadTask {
     let task = server.uploadNewImage(fromURL: photoURL, id: id, completion: completion)
     uploads.append(task)
     UIImage(contentsOfFile: photoURL.path).map {
       self.cache.setImage($0, forId: id)
     }
-    return id
+    return task
   }
 }
