819733cec079eabff43337f69876bf4eda692946
Nicolas Degen <ndegen@me.com>
2022-01-31 12:54:57 +0100

    uiimage squared improvements

--- Sources/Show/Tools/UIImage+Squared.swift
+++ Sources/Show/Tools/UIImage+Squared.swift
@@ -14,22 +14,38 @@ extension UIImage {
   var newX: CGFloat { isLandscape ? ((size.width - size.height) / 2).rounded(.down) : 0 }
   var newY: CGFloat { isPortrait ? ((size.height - size.width) / 2).rounded(.down) : 0 }
 
-  public func squared(size: CGFloat? = nil, isOpaque: Bool = false) -> UIImage? {
-    guard let cgImage = cgImage?
-      .cropping(to: .init(origin: .init(x: newX, y: newY),
-                          size: breadthSize)) else { return nil }
-    let format = imageRendererFormat
-    format.opaque = isOpaque
-    let squared = UIGraphicsImageRenderer(size: breadthSize, format: format).image { _ in
-      UIImage(cgImage: cgImage, scale: 1, orientation: imageOrientation)
-        .draw(in: .init(origin: .zero, size: breadthSize))
-    }
-
-    if let size = size, let resized = squared.resize(clampingMin: size) {
-      return resized
+  public func squared(size: CGFloat? = nil) -> UIImage? {
+    if let size = size {
+      return resize(clampingMin: size)?.cropToSquare()
     } else {
-      return squared
+      return cropToSquare()
     }
+  }
+  
+  func cropToSquare() -> UIImage? {
+    var imageHeight = size.height
+    var imageWidth = size.width
+    
+    if imageHeight > imageWidth {
+      imageHeight = imageWidth
+    } else {
+      imageWidth = imageHeight
+    }
+    
+    let size = CGSize(width: imageWidth, height: imageHeight)
+    
+    let refWidth : CGFloat = CGFloat(cgImage!.width)
+    let refHeight : CGFloat = CGFloat(cgImage!.height)
+    
+    let x = (refWidth - size.width) / 2
+    let y = (refHeight - size.height) / 2
+    
+    let cropRect = CGRect(x: x, y: y, width: size.height, height: size.width)
+    if let imageRef = cgImage?.cropping(to: cropRect) {
+      return UIImage(cgImage: imageRef, scale: 0, orientation: imageOrientation)
+    }
+    
+    return nil
   }
 }
 #endif
